866e947c376042f61f4d23552802568db510ce46
Andriy Polukhin <andriy.polukhin@gmail.com>
2023-11-30 00:14:10 +0200

    feat(listings): added sample data for listing, added routes to list data and delete listing entries

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,27 @@
 import express from 'express'
+import { listings } from './listings'
 const app = express()
 const port = 9000
 
-const one = 1
-const two = 2
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`))
+app.get('/listings', (_req, res) => {
+	return res.send(listings)
+})
+
+app.post('/delete-listing', (req, res) => {
+	const id: string = req.body.id
+
+	listings.map((listing, i) => {
+		if (listing.id === id) {
+			return res.send(listings.splice(i, 1))
+		}
+	})
+
+	return res.send('failed to delete listing')
+})
 
 app.listen(port)
+
 console.log(`[app]: http://localhost:${port}`)
