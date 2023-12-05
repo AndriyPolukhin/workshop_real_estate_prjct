6af06be987b400430253add5c84d83511a1cad8d
Andriy Polukhin <andriy.polukhin@gmail.com>
2023-12-05 22:45:49 +0200

    feat(listings): ui is able to get listings via fetch and to delete listing using a graphql query

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,3 +1,63 @@
-export const Listings = () => {
-	return <h2>Real Estate Listings</h2>
+import { server } from '../../lib/api'
+import {
+	ListingsData,
+	DeleteListingData,
+	DeleteListingVariables,
+} from './types'
+// * Adding a GraphQL query
+const LISTINGS = `
+    query Listings {
+        listings {
+            id
+            title
+            image
+            address
+            price
+            numOfGuests
+            numOfBeds
+            numOfBaths
+            rating
+        }
+    }
+`
+
+const DELETE_LISTING = `
+    mutation DeleteListing($id: ID!) {
+        deleteListing(id: $id) {
+            id
+        }
+    }
+`
+
+interface Props {
+	title: String
+}
+export const Listings = ({ title }: Props) => {
+	const fetchListings = async () => {
+		const { data } = await server.fetch<ListingsData>({ query: LISTINGS })
+
+		return data
+	}
+
+	const deleteListing = async () => {
+		const { data } = await server.fetch<
+			DeleteListingData,
+			DeleteListingVariables
+		>({
+			query: DELETE_LISTING,
+			variables: {
+				id: '656cfc40e6056b602d997d05',
+			},
+		})
+
+		return data
+	}
+
+	return (
+		<div>
+			<h2>{title}</h2>
+			<button onClick={fetchListings}>Query Listings!</button>
+			<button onClick={deleteListing}>Delete a Listing</button>
+		</div>
+	)
 }
