e4765430006773b2cab5472f790b573be8444215
Andriy Polukhin <andriy.polukhin@gmail.com>
2023-12-10 22:49:49 +0200

    feat(router): setup react-router-dom, created initial sections for the route as templates, cleaned up previous temp setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,30 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { Listings } from './sections'
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+	Outlet,
+} from 'react-router-dom'
+import { Home, Host, Listing, Listings, NotFound, User } from './sections'
 import { StyleProvider } from '@ant-design/cssinjs'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
-// import './styles/index.css'
-
-import reportWebVitals from './reportWebVitals'
 
 const client = new ApolloClient({
 	uri: '/api', // "http://localhost:9000"
 	cache: new InMemoryCache(),
 })
 
+const App = () => (
+	<>
+		<Outlet></Outlet>
+	</>
+)
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<App />}>
+			<Route index={true} path='/' element={<Home />} />
+			<Route path='/host' element={<Host />} />
+
+			<Route path='/listing/:id' element={<Listing />} />
+			<Route path='/listings/:location?' element={<Listings />} />
+			<Route path='/user/:id' element={<User />} />
+			<Route path='*' element={<NotFound />} />
+		</Route>
+	)
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
 		<ApolloProvider client={client}>
 			<StyleProvider hashPriority='high'>
-				<Listings title='Real Estate Project' />
+				<RouterProvider router={router} />
 			</StyleProvider>
 		</ApolloProvider>
 	</React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
