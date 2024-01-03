1a3935a068551554802fd629f41f9f9b160abd6f
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-01-03 16:57:43 +0200

    feat(ui): Home page -> added CTA section with various listings from premium to any kind of listings

--- client/src/sections/Home/components/HomeHero/index.tsx
+++ client/src/sections/Home/components/HomeHero/index.tsx
@@ -1,4 +1,3 @@
-import StyleContext from '@ant-design/cssinjs'
 import { Link } from 'react-router-dom'
 import { Card, Col, Row, Input, Typography } from 'antd'
 
@@ -10,103 +9,51 @@ import londonImage from '../../assets/london.jpg'
 const { Title } = Typography
 const { Search } = Input
 
-export const HomeHero = () => {
+interface Props {
+	onSearch: (value: string) => void
+}
+
+export const HomeHero = ({ onSearch }: Props) => {
 	return (
-		<div style={{ width: '100%' }}>
-			<div
-				style={{
-					maxWidth: '680px',
-				}}
-			>
-				<Title style={{ color: '#1d226c' }}>
+		<div className='home-hero'>
+			<div className='home-hero__search'>
+				<Title className='home-hero__title'>
 					Find a place you'll love to stay at
 				</Title>
 				<Search
 					placeholder="Search 'San Fransico'"
 					size='large'
 					enterButton
-					style={homeHeroSearchInputStyle}
+					className='home-hero__search-input'
+					onSearch={onSearch}
 				/>
 			</div>
-			<Row gutter={12} style={{ paddingTop: '60px' }}>
+			<Row gutter={12} className='home-hero__cards'>
 				<Col xs={12} md={6}>
 					<Link to='/listings/toronto'>
-						<Card
-							style={homeHeroCardsStyle}
-							cover={<img alt='Toronto' src={torontoImage} />}
-						>
+						<Card cover={<img alt='Toronto' src={torontoImage} />}>
 							Toronto
 						</Card>
 					</Link>
 				</Col>
 				<Col xs={12} md={6}>
 					<Link to='/listings/dubai'>
-						<Card
-							style={homeHeroCardsStyle}
-							cover={<img alt='Dubai' src={dubaiImage} />}
-						>
-							Dubai
-						</Card>
+						<Card cover={<img alt='Dubai' src={dubaiImage} />}>Dubai</Card>
 					</Link>
 				</Col>
 				<Col xs={0} md={6}>
 					<Link to='/listings/los%20angeles'>
-						<Card
-							style={homeHeroCardsStyle}
-							cover={<img alt='Los Angeles' src={losAgnelesImage} />}
-						>
+						<Card cover={<img alt='Los Angeles' src={losAgnelesImage} />}>
 							Los Angeles
 						</Card>
 					</Link>
 				</Col>
 				<Col xs={0} md={6}>
 					<Link to='/listings/london'>
-						<Card
-							style={{ ...homeHeroCardsStyle, ...homeHeroCardsCoverStyle }}
-							cover={<img alt='London' src={londonImage} />}
-						>
-							London
-						</Card>
+						<Card cover={<img alt='London' src={londonImage} />}>London</Card>
 					</Link>
 				</Col>
 			</Row>
 		</div>
 	)
-}
-
-const homeHeroSearchInputStyle: React.CSSProperties = {
-	border: '1px solid #f0f1f2',
-	boxShadow: '0 2px 4px rgba(29, 34, 108, 0.1)',
-}
-
-const homeHeroCardsStyle: React.CSSProperties = {
-	background: 'transparent',
-	cursor: 'pointer',
-}
-
-const homeHeroCardsCoverStyle = {
-	'.ant-card > div.ant-card-cover .ant-card-cover:before': {
-		content: '',
-		display: 'block',
-		position: 'absolute',
-		top: '0',
-		bottom: '0',
-		left: '0',
-		right: '0',
-		background: '#1d226c',
-		opacity: '0.5',
-		transition: 'all 0.3s linear',
-	},
-	'.ant-card > div.ant-card-cover .ant-card-cover:hover:before': {
-		background: 'none ',
-	},
-	'.ant-card > div.ant-card-cover div.ant-card-body': {
-		paddingTop: '0',
-		paddingBottom: '0',
-		height: '0',
-		color: '#fff',
-		fontWeight: '700',
-		position: 'relative !important',
-		bottom: '40px ',
-	},
 }
--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,18 +1,79 @@
-import { Layout } from 'antd'
+import { Col, Row, Layout, Typography, Button } from 'antd'
+import { useNavigate, Link } from 'react-router-dom'
+import { displayErrorMessage } from '../../lib/utils'
 import { HomeHero } from './components'
 
+import mapBackground from './assets/map-background.jpg'
+import sanFransiscoImage from './assets/san-fransisco.jpg'
+import cancunImage from './assets/cancun.jpg'
+
 const { Content } = Layout
+const { Title, Paragraph } = Typography
+
 export const Home = () => {
+	const navigate = useNavigate()
+	const onSearch = (value: string) => {
+		const trimmedValue = value.trim()
+
+		if (trimmedValue) {
+			navigate(`/listings/${trimmedValue}`)
+		} else {
+			displayErrorMessage('Please enter a valid search')
+		}
+	}
 	return (
-		<Content style={homeStyle}>
-			<HomeHero />
+		<Content
+			className='home'
+			style={{ backgroundImage: `url(${mapBackground})` }}
+		>
+			<HomeHero onSearch={onSearch} />
+
+			<div className='home__cta-section'>
+				<Title level={2} className='home__cta-section-title'>
+					Your guide for all things rental
+				</Title>
+				<Paragraph>
+					Helping you make the last decisions in renting last minute locations.
+				</Paragraph>
+				<Link to='/listings/united%20states'>
+					<Button
+						type='primary'
+						size='large'
+						className='home__cta-section-button'
+					>
+						Popular listings in the United States
+					</Button>
+				</Link>
+			</div>
+			<div className='home__listings'>
+				<Title level={4} className='home__listings-title'>
+					Listings of any kind
+				</Title>
+				<Row gutter={12}>
+					<Col xs={24} sm={12}>
+						<Link to='/listings/san%20fransisco'>
+							<div className='home__listings-img-cover'>
+								<img
+									src={sanFransiscoImage}
+									alt='San Fransisco'
+									className='home__listings-img'
+								/>
+							</div>
+						</Link>
+					</Col>
+					<Col xs={24} sm={12}>
+						<Link to='/listings/cancún'>
+							<div className='home__listings-img-cover'>
+								<img
+									src={cancunImage}
+									alt='Cancún'
+									className='home__listings-img'
+								/>
+							</div>
+						</Link>
+					</Col>
+				</Row>
+			</div>
 		</Content>
 	)
-}
-
-const homeStyle: React.CSSProperties = {
-	padding: '60px 120px',
-	backgroundImage: './assets/map-background.jpg',
-	backgroundRepeat: 'no-repeat',
-	backgroundPosition: '10% 0',
 }
