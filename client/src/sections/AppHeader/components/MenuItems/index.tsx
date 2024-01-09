8157a56d5ba48fbd0bacda1a5a42de66b64f5b88
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-01-09 22:53:30 +0200

    fix: cleaned out duplicate code, added Search component and functionality

--- client/src/sections/AppHeader/components/MenuItems/index.tsx
+++ client/src/sections/AppHeader/components/MenuItems/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import { useMutation } from '@apollo/client'
 import { Avatar, Button, Menu } from 'antd'
@@ -14,15 +13,6 @@ import type { MenuProps } from 'antd'
 interface Props {
 	viewer: Viewer
 	setViewer: (viewer: Viewer) => void
-}
-
-const { Item, SubMenu } = Menu
-
-const MenuStyles: React.CSSProperties = {
-	width: '100%',
-	padding: '0 20px',
-	lineHeight: '63px',
-	border: 0,
 }
 
 export const MenuItems = ({ viewer, setViewer }: Props) => {
@@ -97,71 +87,10 @@ export const MenuItems = ({ viewer, setViewer }: Props) => {
 
 	return (
 		<Menu
-			style={MenuStyles}
+			className='menu'
 			mode='horizontal'
 			selectable={false}
 			items={viewer.id && viewer.avatar ? items2 : items}
 		/>
-	)
-}
-
-export const MenuItemsBackup = ({ viewer, setViewer }: Props) => {
-	const [logOut] = useMutation<LogOutData>(LOG_OUT, {
-		onCompleted: (data) => {
-			if (data && data.logOut) {
-				setViewer({
-					id: data.logOut.id || null,
-					token: data.logOut.token || null,
-					avatar: data.logOut.avatar || null,
-					hasWallet: data.logOut.hasWallet || false,
-					didRequest: data.logOut.didRequest || true,
-				})
-				displaySuccessNotification("You've successfully logged out!")
-			}
-		},
-		onError: (data) => {
-			displayErrorMessage(
-				"Sorry! We weren't able to log you out. Please try again later!"
-			)
-		},
-	})
-
-	const handleLogOut = () => {
-		logOut()
-	}
-	const subMenuLogin =
-		viewer.id && viewer.avatar ? (
-			<SubMenu key={viewer.id} title={<Avatar src={viewer.avatar} />}>
-				<Item key='/user'>
-					<Link to='/user'>
-						<UserOutlined style={{ paddingRight: '5px' }} />
-						Profile
-					</Link>
-				</Item>
-				<Item key='/logout'>
-					<div onClick={handleLogOut}>
-						<LogoutOutlined style={{ paddingRight: '5px' }} />
-						Log out
-					</div>
-				</Item>
-			</SubMenu>
-		) : (
-			<Item key='/login'>
-				<Link to='/login'>
-					<Button type='primary'>Sign In</Button>
-				</Link>
-			</Item>
-		)
-
-	return (
-		<Menu mode='horizontal' selectable={false} style={MenuStyles}>
-			<Item key='/host'>
-				<Link to='/host'>
-					<HomeOutlined style={{ paddingRight: '5px' }} />
-					Host
-				</Link>
-			</Item>
-			{subMenuLogin}
-		</Menu>
 	)
 }
