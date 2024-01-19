87b7a301df6f57c56bcd51437bc3c10698e8379e
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-01-19 22:14:47 +0200

    feat(not-found): added a not-found page to inform and direct users

--- client/src/sections/NotFound/index.tsx
+++ client/src/sections/NotFound/index.tsx
@@ -1,3 +1,26 @@
+import { Link } from 'react-router-dom'
+import { Empty, Layout, Typography, Button } from 'antd'
+
+const { Content } = Layout
+const { Text } = Typography
 export const NotFound = () => {
-	return <div>NotFound</div>
+	return (
+		<Content className='not-found'>
+			<Empty
+				description={
+					<>
+						<Text className='not-found__description-title'>
+							U&h oh! something went wrong :(
+						</Text>
+						<Text className='not-found__description-subtitle'>
+							The page you're looking from can't be found
+						</Text>
+					</>
+				}
+			/>
+			<Button type='primary' size='large' className='not-found__cta'>
+				<Link to='/'>Go to home</Link>
+			</Button>
+		</Content>
+	)
 }
