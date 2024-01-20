0ba449bd08b0d0d64275b92d6fbb0f779b43deab
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-01-20 22:50:15 +0200

    feat(db): updated seed files to bootstrap the pgdb

--- server/temp/clear.ts
+++ server/temp/clear.ts
@@ -1,27 +1,6 @@
 import dotenv from 'dotenv'
+import { connectDatabase } from '../src/database'
 dotenv.config()
-import { Listing, User, Booking, Database } from '../src/lib/types'
-
-import { MongoClient } from 'mongodb'
-
-const connectDatabase = async (): Promise<Database> => {
-	try {
-		const client = await MongoClient.connect(`${process.env.MONGO_URI}`)
-
-		const db = client.db('main')
-		console.log(`
-    💽  MongoDB Connected to db: ${db.namespace}`)
-
-		return {
-			bookings: db.collection<Booking>('bookings'),
-			listings: db.collection<Listing>('listings'),
-			users: db.collection<User>('users'),
-		}
-	} catch (error) {
-		console.log(`Error: ${error}`)
-		process.exit(1)
-	}
-}
 
 const clear = async () => {
 	try {
@@ -30,21 +9,9 @@ const clear = async () => {
 
 		const db = await connectDatabase()
 
-		const bookings = await db.bookings.find({}).toArray()
-		const listings = await db.listings.find({}).toArray()
-		const users = await db.users.find({}).toArray()
-
-		if (bookings.length > 0) {
-			await db.bookings.drop()
-		}
-
-		if (listings.length > 0) {
-			await db.listings.drop()
-		}
-
-		if (users.length > 0) {
-			await db.users.drop()
-		}
+		await db.bookings.clear()
+		await db.listings.clear()
+		await db.users.clear()
 
 		console.log(`
     🏟️ [clear] : successfull`)
--- server/temp/seed.ts
+++ server/temp/seed.ts
@@ -1,28 +1,8 @@
 import dotenv from 'dotenv'
 dotenv.config()
-import { Listing, User, Booking, Database } from '../src/lib/types'
+import { connectDatabase } from '../src/database'
 import { listings } from './listings'
 import { users } from './users'
-import { MongoClient } from 'mongodb'
-
-const connectDatabase = async (): Promise<Database> => {
-	try {
-		const client = await MongoClient.connect(`${process.env.MONGO_URI}`)
-
-		const db = client.db('main')
-		console.log(`
-    💽  MongoDB Connected to db: ${db.namespace}`)
-
-		return {
-			bookings: db.collection<Booking>('bookings'),
-			listings: db.collection<Listing>('listings'),
-			users: db.collection<User>('users'),
-		}
-	} catch (error) {
-		console.log(`Error: ${error}`)
-		process.exit(1)
-	}
-}
 
 const seed = async () => {
 	try {
@@ -32,10 +12,10 @@ const seed = async () => {
 		const db = await connectDatabase()
 
 		for (const listing of listings) {
-			await db.listings.insertOne(listing)
+			await db.listings.create(listing).save()
 		}
 		for (const user of users) {
-			await db.users.insertOne(user)
+			await db.users.create(user).save()
 		}
 
 		console.log(`
